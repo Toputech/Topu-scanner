23400223bd9b20be18a48c9a5823a8d95e497696
Toputech <168991681+Toputech@users.noreply.github.com>
2025-05-15 11:57:02 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id, { text: 'TOPU;;;' + b64data });
+				   let session = await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id, { text: '' + b64data });
 	
 				   let FLASH_MD_TEXT = `
 THANKYOU FOR CHOOSING ALONE MD
