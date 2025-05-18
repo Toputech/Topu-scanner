0001dba8688d427505207e641385cbb2981c1b9d
Toputech <168991681+Toputech@users.noreply.github.com>
2025-05-18 21:14:00 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -11,7 +11,7 @@ const {
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("maher-zubair-baileys");
+} = require("npm:@fizzxydev/baileys-pro@latest");
 
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
